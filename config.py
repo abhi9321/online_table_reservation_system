2fae3fad0a0cc8895d7f733c71df69aadd3b4061
Abhishek <42564964+abhi9321@users.noreply.github.com>
2020-07-28 10:37:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     # set configs
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    sender_email_id = 'agopalaiah@quotient.com'
+    sender_email_id = 'agopalaiah@gmail.com'
     sender_email_id_password = 'Quojan@123'
     # receiver_email_id = 'agopalaiah@quotient.com'
 
